e99573c90af2e6218e45fb01cca1b25a93497caf
ShchMax-sudo <Sherbonos.maksim@yandex.ru>
2022-02-15 14:09:46 +0300

    Added decolor command.

--- quirks/defaults.py
+++ quirks/defaults.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 def upperrep(text):
     return text.upper()
@@ -15,3 +16,28 @@ scramblerep.command = "scramble"
 def reverserep(text):
     return text[::-1]
 reverserep.command = "reverse"
+
+def removecolors(texts):
+    matches = re.finditer("<c=(.+?)>(.+?)</c>", texts)
+    answer = [(ch, None) for ch in texts]
+    for match in matches:
+        ms = texts[match.start():match.end()]
+        colour = re.search("=(.+?)>", ms).group(0)[1:-1]
+        text = re.search(">(.+?)<", ms).group(0)[1:-1]
+        answer[match.start()] = (text, colour)
+        for i in range(match.start() + 1, match.end()):
+            answer[i] = (None, None)
+    text = ""
+    iscolour = True
+    for (ch, col) in answer:
+        if (ch == None):
+            continue
+        elif (ch == "|"):
+            iscolour = not iscolour
+        else:
+            if (iscolour and col != None):
+                text += "<c=" + col + ">" + ch + "</c>"
+            else:
+                text += ch
+    return text
+removecolors.command = "decolor"
