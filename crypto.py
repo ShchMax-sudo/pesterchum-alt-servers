1a78063074a89cab7fc22ec7e14ab1eb0af7f786
ShchMax-sudo <Sherbonos.maksim@yandex.ru>
2022-02-18 16:50:49 +0300

    Crypto now in class

--- crypto.py
+++ crypto.py
@@ -4,234 +4,222 @@ import binascii
 from random import randint
 
 
-dcsymbhol = '🔓'  # Don't encrypted
-ecsymbhol = '🔒'  # Encrypted
-ErrorKeyNotFound = "<c=red>Error: Not found open rsa key</c>"
-chunksize = 1024 // 16
-n = 9173503
-d = 6111579
-e = 3
+class Encoder():
+    dcsymbol = '🔓'   # Don't encrypted
+    ecsymbol = '🔒'   # Encrypted
+    keysymbol = '🔑'  # Keys
+    chunksize = 64
+    n = None
+    d = None
+    e = None
 
+    def binPow(self, a, p, n):
+        res = 1
+        while (p):
+            if (p & 1):
+                res *= a
+                res %= n
+            a *= a
+            a %= n
+            p >>= 1
+        return res
 
-def binPow(a, p, n):
-    res = 1
-    while (p):
-        if (p & 1):
-            res *= a
-            res %= n
-        a *= a
-        a %= n
-        p >>= 1
-    return res
+    def factorN(self, n):
+        s = 0
+        while (n % 2 == 0):
+            s += 1
+            n //= 2
+        return (s, n)
 
-
-def factorN(n):
-    s = 0
-    while (n % 2 == 0):
-        s += 1
-        n //= 2
-    return (s, n)
-
-
-def iterTest(a, s, m, n):
-    q = binPow(a, m, n)
-    if (abs(q) == 1):
-        return True
-    for i in range(s - 1):
-        q = (q * q) % n
-        if (q == n - 1):
+    def iterTest(self, a, s, m, n):
+        q = self.binPow(a, m, n)
+        if (abs(q) == 1):
             return True
-    return False
-
-
-def isPrime(n, k):
-    if (n % 2 == 0):
+        for i in range(s - 1):
+            q = (q * q) % n
+            if (q == n - 1):
+                return True
         return False
-    s, m = factorN(n - 1)
-    for i in range(k):
-        if (not iterTest(randint(2, n - 1), s, m, n)):
+
+    def isPrime(self, n, k):
+        if (n % 2 == 0):
             return False
-    return True
+        s, m = self.factorN(n - 1)
+        for i in range(k):
+            if (not self.iterTest(randint(2, n - 1), s, m, n)):
+                return False
+        return True
 
-
-def m_r_gen(k, size):
-    pr = randint((1 << (size - 1)) + 1, (1 << (size)) - 1)
-    while (not isPrime(pr, k)):
+    def primeGen(self, k, size):
         pr = randint((1 << (size - 1)) + 1, (1 << (size)) - 1)
-    return pr
+        while (not self.isPrime(pr, k)):
+            pr = randint((1 << (size - 1)) + 1, (1 << (size)) - 1)
+        return pr
 
+    def gcd(self, a, b):
+        if (a == 0):
+            x = 0
+            y = 1
+            return (x, y, b)
+        x1, y1, d = self.gcd(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1
+        return (x, y, d)
 
-def gcd(a, b):
-    if (a == 0):
-        x = 0
-        y = 1
-        return (x, y, b)
-    x1, y1, d = gcd(b % a, a)
-    x = y1 - (b // a) * x1
-    y = x1
-    return (x, y, d)
+    def inv(self, k, m):
+        return self.gcd(k, -m)[0] % m
 
-
-def inv(k, m):
-    return gcd(k, -m)[0] % m
-
-
-def rsa_keygen():
-    p = m_r_gen(30, 128)
-    q = m_r_gen(30, 128)
-    while (q == p):
-        q = m_r_gen(30, 128)
-    n = p * q
-    phi = (p - 1) * (q - 1)
-    e = randint(1, phi - 1)
-    while (gcd(e, phi)[2] != 1):
+    def rsaKeygen(self):
+        p = self.primeGen(30, 128)
+        q = self.primeGen(30, 128)
+        while (q == p):
+            q = self.primeGen(30, 128)
+        n = p * q
+        phi = (p - 1) * (q - 1)
         e = randint(1, phi - 1)
-    d = inv(e, phi)
-    return (n, p, q, e, d)
+        while (self.gcd(e, phi)[2] != 1):
+            e = randint(1, phi - 1)
+        d = self.inv(e, phi)
+        self.n = n
+        self.e = e
+        self.d = d
 
+    def int15(self, num):
+        preans = []
+        nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B",
+                "C", "D", "E"]
+        while (num != 0):
+            preans.append(nums[num % 15])
+            num //= 15
+        return "".join(list(reversed(preans)))
 
-def int15(num):
-    preans = []
-    nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B",
-            "C", "D", "E"]
-    while (num != 0):
-        preans.append(nums[num % 15])
-        num //= 15
-    return "".join(list(reversed(preans)))
+    def int15to16(self, str):
+        d = {
+            "0": "1",
+            "1": "2",
+            "2": "3",
+            "3": "4",
+            "4": "5",
+            "5": "6",
+            "6": "7",
+            "7": "8",
+            "8": "9",
+            "9": "A",
+            "A": "B",
+            "B": "C",
+            "C": "D",
+            "D": "E",
+            "E": "F",
+        }
+        return "".join([d[ch.upper()] for ch in str])
 
+    def int16to15(self, str):
+        d = {
+            "1": "0",
+            "2": "1",
+            "3": "2",
+            "4": "3",
+            "5": "4",
+            "6": "5",
+            "7": "6",
+            "8": "7",
+            "9": "8",
+            "A": "9",
+            "B": "A",
+            "C": "B",
+            "D": "C",
+            "E": "D",
+            "F": "E",
+        }
+        return "".join([d[ch.upper()] for ch in str])
 
-def int15to16(str):
-    d = {
-        "0": "1",
-        "1": "2",
-        "2": "3",
-        "3": "4",
-        "4": "5",
-        "5": "6",
-        "6": "7",
-        "7": "8",
-        "8": "9",
-        "9": "A",
-        "A": "B",
-        "B": "C",
-        "C": "D",
-        "D": "E",
-        "E": "F",
-    }
-    return "".join([d[ch.upper()] for ch in str])
-
-
-def int16to15(str):
-    d = {
-        "1": "0",
-        "2": "1",
-        "3": "2",
-        "4": "3",
-        "5": "4",
-        "6": "5",
-        "7": "6",
-        "8": "7",
-        "9": "8",
-        "A": "9",
-        "B": "A",
-        "C": "B",
-        "D": "C",
-        "E": "D",
-        "F": "E",
-    }
-    return "".join([d[ch.upper()] for ch in str])
-
-
-def strToHex(string):
-    return int15to16(
-        int15(
-            int(
-                binascii.hexlify(
-                    string.encode("utf-8")
-                ).decode("ascii"), base=16
+    def strToHex(self, string):
+        return self.int15to16(
+            self.int15(
+                int(
+                    binascii.hexlify(
+                        string.encode("utf-8")
+                    ).decode("ascii"), base=16
+                )
             )
         )
-    )
 
+    def hexToStr(self, hexstr):
+        return binascii.unhexlify(
+            hex(int(self.int16to15(hexstr), base=15))[2:].encode("ascii")
+        ).decode("utf-8")
 
-def hexToStr(hexstr):
-    return binascii.unhexlify(
-        hex(int(int16to15(hexstr), base=15))[2:].encode("ascii")
-    ).decode("utf-8")
+    def encodeChunk(self, chunk):
+        chunk = int(chunk, base=16)
+        return (("0" * self.chunksize)
+                + hex(self.binPow(chunk,
+                                  self.e, self.n))[2:])[-self.chunksize:]
 
+    def decodeChunk(self, chunk):
+        chunk = int(chunk, base=16)
+        return hex(self.binPow(chunk, self.d, self.n))[2:]
 
-def encodeChunk(chunk):
-    chunk = int(chunk, base=16)
-    return (("0" * chunksize) + hex(binPow(chunk, e, n))[2:])[-chunksize:]
+    def decodeMessage(self, message):
+        cryptosymbhol = message[0]
+        if (cryptosymbhol == self.dcsymbol):
+            return message[1:]
+        elif (cryptosymbhol != self.ecsymbol):
+            return message
+        else:
+            cryptedmessage = message[1:]
+            decryptedmessage = ""
+            for i in range(0, len(cryptedmessage), self.chunksize):
+                decryptedmessage += self.decodeChunk(
+                    cryptedmessage[i:i + self.chunksize])
+            return self.hexToStr(decryptedmessage)
 
+    def canEncode(self):
+        return self.e is not None
 
-def decodeChunk(chunk):
-    chunk = int(chunk, base=16)
-    return hex(binPow(chunk, d, n))[2:]
+    def encodeMessage(self, message, user):
+        if (not self.canEncode()):
+            return self.dcsymbol + message
+        hexed = self.strToHex(message)
+        cryptedmessage = ""
+        i = 0
+        while (i < len(hexed)):
+            currentChunk = ""
+            while (i < len(hexed)
+                   and int(currentChunk + hexed[i], base=16) < self.n):
+                currentChunk += hexed[i]
+                i += 1
+            cryptedmessage += self.encodeChunk(currentChunk)
+        return self.ecsymbol + cryptedmessage
 
+    def encodeKeys(self):
+        return self.keysymbol \
+            + hex(self.e)[2:] + " " + hex(self.n)[2:] + self.keysymbol
 
-def getKey(user=None):
-    # """
-    if (user is None):
-        return (d, n)
-    else:
-        return (e, n)
-    # """
-    _rsadir = ostools.getDataDir() + "rsa/"
-    if (not os.path.exists(_rsadir)):
-        os.mkdir(_rsadir)
-    beg = 0
-    end = 2
-    if (user is None):
-        user = "key"
-    elif (not isinstance(user, str)):
-        user = "key"
-        beg = 2
-        end = 4
-    if (not os.path.exists(_rsadir + user + ".dat")):
-        return (None, None)
-    return list(map(int, open(_rsadir + user + ".dat", "w").readlines()))[beg:end]
-
-
-def decodeMessage(message):
-    cryptosymbhol = message[0]
-    if (cryptosymbhol == dcsymbhol):
-        return message[1:]
-    elif (cryptosymbhol != ecsymbhol):
-        return message
-    else:
-        cryptedmessage = message[1:]
-        (d, n) = getKey()
-        if (d is None):
-            return ErrorKeyNotFound
-        decryptedmessage = ""
-        for i in range(0, len(cryptedmessage), chunksize):
-            decryptedmessage += decodeChunk(
-                cryptedmessage[i:i + chunksize])
-        return hexToStr(decryptedmessage)
-
-
-def encodeMessage(message, user):
-    (e, n) = getKey(user)
-    if (e is None):
-        return dcsymbhol + message
-    hexed = strToHex(message)
-    cryptedmessage = ""
-    i = 0
-    while (i < len(hexed)):
-        currentChunk = ""
-        while (i < len(hexed) and int(currentChunk + hexed[i], base=16) < n):
-            currentChunk += hexed[i]
-            i += 1
-        cryptedmessage += encodeChunk(currentChunk)
-    return ecsymbhol + cryptedmessage
+    def decodeKeys(self, message):
+        if (message[0] != message[-1] or message[0] != self.keysymbol):
+            return False
+        e_ = None
+        n_ = None
+        try:
+            e_, n_ = map(lambda x: int(x, base=16), message[1:-1].split())
+        except ValueError:
+            return False
+        if (e_ is None or n_ is None):
+            return False
+        self.e = e_
+        self.n = n_
 
 
 if (__name__ == "__main__"):
     message = "TestTestTestWith🔒"
     user = "ShchMax"
-    cr = encodeMessage(message, user)
+    en = Encoder()
+    en.rsaKeygen()
+    cr = en.encodeMessage(message, user)
     print(cr)
-    print(decodeMessage(cr))
-    print(encodeChunk("123F"))
-    print(decodeChunk(encodeChunk("123F")))
+    print(en.decodeMessage(cr))
+    print(en.encodeChunk("123F"))
+    print(en.decodeChunk(en.encodeChunk("123F")))
+    print(en.encodeKeys())
+    en.decodeKeys(en.encodeKeys())
+    print(en.decodeMessage(cr))
