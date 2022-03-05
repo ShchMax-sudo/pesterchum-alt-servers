baf848347845eddc64915abcc558697023f970b7
ShchMax-sudo <Sherbonos.maksim@yandex.ru>
2022-03-05 12:06:50 +0300

    Fixed crypto decode keys bug

--- crypto.py
+++ crypto.py
@@ -248,7 +248,7 @@ class Encoder():
                or ns[int(ns[i], base=16)] != ch[i + 5]):
                 return False
         name = self.hexToStr(name)
-        if (name == myname):
+        if (name == myname and isPuch):
             return False
         if (isPush):
             self.e = e_
