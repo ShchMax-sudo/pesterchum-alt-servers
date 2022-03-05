f8a2bcf93fba66d2b6d7587fb9e784e9271c3f26
ShchMax-sudo <Sherbonos.maksim@yandex.ru>
2022-03-05 12:15:15 +0300

    Updated crypto and decolor in defaults

--- quirks/defaults.py
+++ quirks/defaults.py
@@ -19,17 +19,5 @@ reverserep.command = "reverse"
 
 # Removes color from text so it becomes readable after affect of some questionable quirks.
 def removecolors(texts):
-    matches = re.finditer("<c=(.+?)>(.+?)</c>", texts)
-    answer = [(ch, None) for ch in texts]
-    for match in matches:
-        ms = texts[match.start():match.end()]
-        colour = re.search("=(.+?)>", ms).group(0)[1:-1]
-        text = re.search(">(.+?)<", ms).group(0)[1:-1]
-        answer[match.start()] = (text, colour)
-        for i in range(match.start() + 1, match.end()):
-            answer[i] = (None, None)
-    text = ""
-    for (ch, col) in answer:
-        text += ch
-    return text
+    return "".join([k[1] for k in re.findall("<c=(.+?)>(.+?)</c>", texts)])
 removecolors.command = "decolor"
